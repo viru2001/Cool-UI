74d3296491961629032ee68b759fab0aa11e9062
Viresh <vireshfegade@gmail.com>
2022-02-09 16:11:06 +0530

    refactor : add comments in modal.js

--- components/Modal/modal.js
+++ components/Modal/modal.js
@@ -1,18 +1,21 @@
+// take refernece of html elements
 const simpleModalWrapper = document.querySelector("#simple-modal-wrapper");
 const simpleModalDemoBtn = document.querySelector("#simple-modal-demo-btn");
 const listModalWrapper = document.querySelector("#list-modal-wrapper");
 const listModalDemoBtn = document.querySelector("#list-modal-demo-btn");
 const actionBtns = document.querySelectorAll(".btn-action");
-
 const body = document.querySelector("body");
 
+// event listener for live demo button for simple modal
 simpleModalDemoBtn.addEventListener("click", () => {
   simpleModalWrapper.style.display = "flex";
 });
-
+// event listener for live demo button for list modal
 listModalDemoBtn.addEventListener("click", () => {
   listModalWrapper.style.display = "flex";
 });
+
+// event listeners for buttons in modal
 actionBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     if (btn.parentNode.id === "simple-modal-buttons") {
@@ -23,10 +26,11 @@ actionBtns.forEach(btn => {
   });
 });
 
+//event listener for closing modal when it is open and we click anywhere on screen/window
 window.addEventListener("click", event => {
   if (event.target === simpleModalWrapper) {
     simpleModalWrapper.style.display = "none";
   } else if (event.target === listModalWrapper) {
     listModalWrapper.style.display = "none";
-  } 
+  }
 });
