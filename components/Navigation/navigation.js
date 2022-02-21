cef56dab87db8ab9b2909c6c0e193fd3c0728486
Viresh <vireshfegade@gmail.com>
2022-02-22 02:32:41 +0530

    refactor : update component navigation menuand make it responsive

--- components/Navigation/navigation.js
+++ components/Navigation/navigation.js
@@ -1,7 +1,7 @@
 const hamburgerMenuBtn = document.querySelector("#hamburger-btn");
 const hamburgerMenu = document.querySelector("#hamburger-nav-menu");
 
-// toggle dispay none class
+// toggle display none class
 hamburgerMenuBtn.addEventListener("click", () => {
   const classes = hamburgerMenu.classList;
   classes.toggle("d-none");
