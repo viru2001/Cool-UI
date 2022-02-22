ec5cc0ad2a5b78b0223596e0e28508dbef8cadfa
Viresh <vireshfegade@gmail.com>
2022-02-22 18:43:36 +0530

    refactor : update mobile navigation on docs page

--- js/docs-nav.js
+++ js/docs-nav.js
@@ -1,9 +1,18 @@
 const hamburgMenuBtn = document.querySelector("#btn-hamburger");
 const hamburgMenu = document.querySelector("#nav-menu-hamburger");
 
+const compNavLinks = document.querySelectorAll(".nav-link-item");
+
 // toggle display none class
 hamburgMenuBtn.addEventListener("click", () => {
   const classes = hamburgMenu.classList;
   classes.toggle("d-none");
+});
 
+// hide component navigation after selecting link from nav menu
+compNavLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    const classes = hamburgMenu.classList;
+    classes.toggle("d-none");
+  });
 });
